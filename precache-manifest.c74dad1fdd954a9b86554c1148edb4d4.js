1b20d1ddfa0e5d146be2422760d84f0bf8bbdc05
kashishnagpal <kashishnagpal10@gmail.com>
2020-10-09 02:45:57 +0530

    Updates

---
+++ precache-manifest.c74dad1fdd954a9b86554c1148edb4d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229d4a63b22a83514ed1fc1e6700eab8",
+    "url": "/Alinea_Task/index.html"
+  },
+  {
+    "revision": "17811e0234a9546e1174",
+    "url": "/Alinea_Task/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3327947c96f867193df8",
+    "url": "/Alinea_Task/static/css/main.9b3ad2fb.chunk.css"
+  },
+  {
+    "revision": "17811e0234a9546e1174",
+    "url": "/Alinea_Task/static/js/2.ef2b3bca.chunk.js"
+  },
+  {
+    "revision": "58c4332868ceba0965c061db8401595f",
+    "url": "/Alinea_Task/static/js/2.ef2b3bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3327947c96f867193df8",
+    "url": "/Alinea_Task/static/js/main.e2514ee2.chunk.js"
+  },
+  {
+    "revision": "e7e2795744a531356103",
+    "url": "/Alinea_Task/static/js/runtime-main.142690ae.js"
+  }
+]);
